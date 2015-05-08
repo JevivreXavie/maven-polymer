9724cf8c2a616e3b94186e87183d305c85e40303
Jevivre xavie <jevivre.xavie@gmail.com>
2015-05-08 17:28:18 -0500

    * Añadido modulo vulcanize

--- maven-polymer/GruntFile.js
+++ maven-polymer/GruntFile.js
@@ -1,26 +1,39 @@
-module.exports = function( grunt ){
+module.exports = function (grunt) {
 
-   // tell grunt to load jshint task plugin.
-   grunt.loadNpmTasks('grunt-contrib-jshint');
+    // tell grunt to load jshint task plugin.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-vulcanize');
 
-   // configure tasks
-   grunt.initConfig({
-      jshint: {
-          files: [
-             'GruntFile.js',
-             'src/main/webapp/scripts/*.js',
-             'src/test/javascript/*.js'
-          ],
-          options: {
-             ignores: [
-                'src/main/webapp/bower_components/*.js'
-             ]
-          }
-      }
-      // more plugin configs go here.
-   });
-
-   grunt.registerTask('default',['jshint']);
+    // configure tasks
+    grunt.initConfig({
+        jshint: {
+            files: [
+                'GruntFile.js',
+                'src/main/webapp/scripts/*.js',
+                'src/test/javascript/*.js'
+            ],
+            options: {
+                ignores: [
+                    'src/main/webapp/bower_components/*.js'
+                ]
+            }
+        },
+        vulcanize: {
+            options: {
+                csp: true,
+                excludes: {
+                    imports: [
+                        "polymer.html"
+                    ]
+                }
+            },
+            files: {
+                'build-csp.html': 'index.html'
+            }}
+        // more plugin configs go here.
+    });
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['vulcanize']);
 
 };
 
